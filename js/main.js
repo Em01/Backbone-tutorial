035ee46b9377de85b7459521a2a17624653aec6f
Em01 <emma_williams1@hotmail.co.uk>
2015-06-08 18:15:08 +0100

    Added collections to main js

--- js/main.js
+++ js/main.js
@@ -1,39 +1,51 @@
-// var Vehicle = Backbone.Model.extend({
-// 	urlRoot: "/api/vehicles",
-// 	validate: function(attrs){
-// 		if (!attr.registrationNumber)
-// 			return "Registration required.";
-// 	},
-// 	start: function() {
-// 			console.log("Vehicle started")
-// 	},
-// });
-
-
-// var Car = Vehicle.extend({
-//   start: function() {
-//     console.log("Car with registration number " + this.get('registrationNumber') + " started.");
-//   }
-// });
-
-// var car = new Car({
-// 	registrationNumber: "XLI887",
-// 	color: "Blue"
-// });
-
-
-var Song = Backbone.Model.extend();
-
-var Songs = Backbone.Collection.extend({
-	model: Song
+var Vehicle = Backbone.Model.extend({
+	urlRoot: "/api/vehicles",
+	validate: function(attrs){
+		if (!attr.registrationNumber)
+			return "Registration required.";
+	},
+	start: function() {
+			console.log("Vehicle started")
+	},
 });
 
-var songs = new Songs();
 
-songs.add(new Song({ title: "Song 1", genre: "Jazz", downloads: 110}), { at: 0});
+var Car = Vehicle.extend({
+  start: function() {
+    console.log("Car with registration number " + this.get('registrationNumber') + " started.");
+  }
+});
 
-songs.push(new Song({ title: "Song 2", genre: "Jazz", downloads: 90}));
+var car = new Car({
+	registrationNumber: "XLI887",
+	color: "Blue"
+});
 
-var jazzSongs = songs.where({ genre: "Jazz" });
+var Vehicle = Backbone.Collection.extend({
+	model: Vehicle,
+});
 
-var firstJazzSong = songs.findWhere({ genre: "Jazz" });
+var vehicles = new Vehicles([
+	new Vehicle({ registrationNumber = "XLI887", color = "Blue"}),
+	new Vehicle({ registrationNumber = "ZNP123", color = "Blue"}),
+	new Vehicle({ registrationNumber = "XUV456", color = "Gray"})
+	));
+
+//find all the blue cols and console log
+var blueCars = vehicles.where({ color: "Blue" });
+console.log(blueCars);
+
+// Find the car with the registration number XLI887 and log it in the console
+var xliCar = vehicles.findWhere({ registrationNumber: "XLI887" });
+console.log(xliCar); 
+
+// Remove that car from the collection
+vehicles.remove(xliCar);
+
+// Convert the collection to a JSON object and log it in the console
+console.log(vehicles.toJSON());
+
+// Iterate through the collection and log each car in the console
+vehicles.each(function(vehicle) {
+	console.log(vehicle);
+});
