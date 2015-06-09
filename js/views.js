b5800f2c605c26d662a41622434f4c9bfb9a2158
Em01 <emma_williams1@hotmail.co.uk>
2015-06-09 14:28:58 +0100

    Handling DOM events

--- js/views.js
+++ js/views.js
@@ -34,39 +34,75 @@
 // $("#container").html(songView.$el);
 
 
+
+
+
 //passing data to views
+// var Song = Backbone.Model.extend();
+
+// var Songs = Backbone.Collection.extend({
+// 	model: Song
+// });
+
+// var SongView = Backbone.View.extend({
+
+// 	render: function() {
+// 		this.$el.html(this.model.get("title"));
+
+// 		return this;
+// 	}
+// });
+
+// var SongsView = Backbone.View.extend({
+// 	render: function(){
+// 		var self = this;
+
+// 		this.model.each(function(song){
+// 			var songView = new SongView({ model: song });
+// 			self.$el.apprend(songView.render().$el);
+// 		});
+
+// 	}
+// });
+
+// var songs = new Songs([
+// 	new Song({ title: "Blue in Green" }),
+// 	new Song({ title: "Title 2"}),
+// 	new Song({ title: "All blues"})
+// 	]);
+
+// var songsView = new SongsView({ el: "#songs", model: songs });
+// songsView
+
+
+//Handling DOM events
+
 var Song = Backbone.Model.extend();
 
-var Songs = Backbone.Collection.extend({
-	model: Song
-});
-
 var SongView = Backbone.View.extend({
+	events: {
+		"click": "onClick",
+		"click .bookmark": "onClickBookmark",
+	},
+
+	onClick: function(){
+		console.log("Listen Clicked");
+	},
+
+	onClickBookmark: function(e){
+		e.stopPropagation();
+		
+		console.log("Bookmark Clicked");
+	},
 
 	render: function() {
-		this.$el.html(this.model.get("title"));
+		this.$el.html(this.model.get("title") + " <button>Listen</button> <button class='bookmark</button>");
 
 		return this;
 	}
 });
 
-var SongsView = Backbone.View.extend({
-	render: function(){
-		var self = this;
+var song = new Song({ title: "Blue in Green" });
 
-		this.model.each(function(song){
-			var songView = new SongView({ model: song });
-			self.$el.apprend(songView.render().$el);
-		});
-
-	}
-});
-
-var songs = new Songs([
-	new Song({ title: "Blue in Green" }),
-	new Song({ title: "Title 2"}),
-	new Song({ title: "All blues"})
-	]);
-
-var songsView = new SongsView({ el: "#songs", model: songs });
-songsView
+var SongView = new SongView({ el: "#container", model: song });
+songsView.render();
